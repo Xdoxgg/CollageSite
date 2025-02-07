a6da406fe12a5bddf9a5d4eebae6ab8caba25fdd
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-02-07 22:51:00 +0300

    add scripts

---
+++ Scripts/main.go
@@ -0,0 +1,51 @@
+﻿package main
+
+import (
+	//"fmt"
+	"database/sql"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Id       int
+	Name     string
+	Password string
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("Pages/timeTable.html")
+	if tmpl != nil {
+		tmpl.Execute(w, nil)
+	}
+}
+
+func contact_page(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contact_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+
+	db, err := sql.Open("posgresql", "root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO users(id, name, password) VALUES(1,'12','52')")
+
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	handleRequest()
+
+}
