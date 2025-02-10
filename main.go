e35a720452686013b4e7c274c3cfbc58aa876720
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-02-10 22:34:50 +0300

    fix styles

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 ﻿package main
 
 import (
-	//"fmt"
-	//"database/sql"
-	 "html/template"
+	"html/template"
 	"net/http"
 )
 
-// type User struct {
-// 	Id       int
-// 	Name     string
-// 	Password string
-// }
+func timeTable(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("Pages/timeTable.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
 
-func home_page(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("Pages/index.html")
 	if err != nil {
 		panic(err)
 	}
 	tmpl.Execute(w, nil)
-	
+	tmpl.ExecuteTemplate(w, "index", nil)
 }
 
-
-
 func handleRequest() {
-	http.HandleFunc("/", home_page)
-	
+	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
+	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/timeTable", timeTable)
 	http.ListenAndServe(":8080", nil)
 }
 
