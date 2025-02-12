e2e9be40d7b3bc0aadb96a6035905fb028ae7c92
Xdox <m.zhuk007@gmail.com>
2025-02-12 23:00:36 +0300

    producing of db

--- DataBase/schema.sql
+++ DataBase/schema.sql
@@ -1,8 +1,28 @@
+-- DROP TABLE IF EXISTS students CASCADE;
+-- DROP TABLE IF EXISTS lessons CASCADE;
 
 
-CREATE TABLE IF NOT EXISTS lessons(
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    lesson_number INTEGER,
-    lesson_date VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS groups (
+   id SERIAL PRIMARY KEY,
+    group_name VARCHAR(255) NOT NULL,
+    
 );
+
+
+CREATE TABLE IF NOT EXISTS students (
+    id SERIAL PRIMARY KEY,
+    group_id INTEGER UNIQUE,
+    student_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+
+);
+
+
+-- CREATE TABLE IF NOT EXISTS lessons(
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(255) NOT NULL,
+--     lesson_number INTEGER,
+--     lesson_date VARCHAR(255) NOT NULL
+-- );
+
+
