7013801f05b99a4a87696140317c18ba14f2dafb
Xdoxgg <m.zhk007@gmail.com>
2025-02-15 09:22:40 +0300

    continue to add db

--- main.go
+++ main.go
@@ -3,16 +3,44 @@
 import (
 	"html/template"
 	"net/http"
+	"database/sql"
+    "github.com/lib/pq""database/sql"
+    "github.com/lib/pq"
 )
 
-// func timeTable(w http.ResponseWriter, r *http.Request) {
-// 	tmpl, err := template.ParseFiles("Pages/timeTable.html")
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	tmpl.Execute(w, nil)
-// }
+
+type Group struct {
+    ID        int    `json:"id"`         
+    GroupName string `json:"group_name"` 
+}
+
+type Students struct { 
+	ID        	int	   `json:"id"` 
+	GroupID  	int    `json:"group_id"`
+	StudentName string `json:"student_name"`
+}
+
+
+type Lessons struct {
+	ID	  		 int 	`json:"id"`
+	Title 		 string `json:"title"`
+	LessonNumber int 	`json:"lesson_number"`
+	LessonDate	 int 	`jspn:"lesson_date"`
+
+}
+
+func connectDB() (*sql.DB, error) {
+	connStr := "user= password= dbname= sslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        fmt.Println("Ошибка подключения к базе данных:", err)
+        return
+    }
+    defer db.Close()
+	
+	
+	return sql.Open("postgres", connStr)
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 
@@ -28,12 +56,33 @@ func handleRequest() {
 	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
 	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
 	http.HandleFunc("/", index)
-	//http.HandleFunc("/timeTable", timeTable)
+	
 	http.ListenAndServe(":8080", nil)
+}
+
+func GetStudents(db *sql.DB) ([]Student, error) {
+    rows, err := db.Query(`SELECT id, group_id, student_name FROM students`)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var students []Student
+    for rows.Next() {
+        var student Student
+        err := rows.Scan(&student.ID, &student.GroupID, &student.StudentName)
+        if err != nil {
+            return nil, err
+        }
+        students = append(students, student)
+    }
+    return students, nil
 }
 
 func main() {
 
 	handleRequest()
+	connectDB()
+
 
 }
