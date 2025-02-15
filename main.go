2512d090be58d1e6397cc2689ee79aa7f4d980cd
Xdoxgg <m.zhk007@gmail.com>
2025-02-15 10:56:08 +0300

    connect db

--- main.go
+++ main.go
@@ -1,88 +1,93 @@
 ﻿package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
-	"database/sql"
-    "github.com/lib/pq""database/sql"
-    "github.com/lib/pq"
+
+	_ "github.com/lib/pq" // Используем "_" для импорта драйвера pq
 )
 
-
 type Group struct {
-    ID        int    `json:"id"`         
-    GroupName string `json:"group_name"` 
+	ID        int    `json:"id"`
+	GroupName string `json:"group_name"`
 }
 
-type Students struct { 
-	ID        	int	   `json:"id"` 
-	GroupID  	int    `json:"group_id"`
+type Student struct {
+	ID          int    `json:"id"`
+	GroupID     int    `json:"group_id"`
 	StudentName string `json:"student_name"`
 }
 
-
-type Lessons struct {
-	ID	  		 int 	`json:"id"`
-	Title 		 string `json:"title"`
-	LessonNumber int 	`json:"lesson_number"`
-	LessonDate	 int 	`jspn:"lesson_date"`
-
+type Lesson struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	LessonNumber int    `json:"lesson_number"`
+	LessonDate   int    `json:"lesson_date"`
 }
 
 func connectDB() (*sql.DB, error) {
-	connStr := "user= password= dbname= sslmode=disable"
+	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
     db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        fmt.Println("Ошибка подключения к базе данных:", err)
-        return
-    }
-    defer db.Close()
-	
-	
-	return sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Ошибка подключения к базе данных:", err)
+		return nil, err
+	}
+	// Проверяем соединение
+	if err = db.Ping(); err != nil {
+		fmt.Println("Не удалось подключиться к базе данных:", err)
+		return nil, err
+	}
+	return db, nil
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
 	tmpl, err := template.ParseFiles("Pages/index.html")
 	if err != nil {
-		panic(err)
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
 	}
 	tmpl.Execute(w, nil)
-	tmpl.ExecuteTemplate(w, "index", nil)
 }
 
 func handleRequest() {
 	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
 	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
 	http.HandleFunc("/", index)
-	
-	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
 
-func GetStudents(db *sql.DB) ([]Student, error) {
-    rows, err := db.Query(`SELECT id, group_id, student_name FROM students`)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+func GetStudents(db *sql.DB) ([]Group, error) {
+	rows, err := db.Query(`SELECT id, group_name FROM groups`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    var students []Student
-    for rows.Next() {
-        var student Student
-        err := rows.Scan(&student.ID, &student.GroupID, &student.StudentName)
-        if err != nil {
-            return nil, err
-        }
-        students = append(students, student)
-    }
-    return students, nil
+	var groups []Group
+	for rows.Next() {
+		var group Group
+		err := rows.Scan(&group.ID, &group.GroupName)
+		if err != nil {
+			return nil, err
+		}
+		groups = append(groups, group)
+	}
+	return groups, nil
 }
 
 func main() {
+	db, err := connectDB()
+	if err != nil {
+		fmt.Println("Не удалось подключиться к базе данных. Завершение работы...")
+		return
+	}
+	defer db.Close()
 
 	handleRequest()
-	connectDB()
-
-
 }
