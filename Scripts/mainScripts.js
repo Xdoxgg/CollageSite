9e5a2d0bab78924ad328c76a5692684e98d98d85
Xdoxgg <m.zhuk007@gmail.com>
2025-02-28 12:33:59 +0300

    temp c

--- Scripts/mainScripts.js
+++ Scripts/mainScripts.js
@@ -8,3 +8,45 @@ function getItemFromLocalStorage(key) {
       }
     });
   }
+
+
+
+  async function setUserData() {
+        
+    user = await getItemFromLocalStorage('user');
+    alert(user)
+    try{
+        user = JSON.parse(user)
+ 
+    }
+    catch (ex){
+        alert(ex)
+    }
+    try {
+     
+        const response = await fetch('/api/stuent_data?s_name=' + encodeURIComponent(user.uName)+'&s_password='+encodeURIComponent(user.uDate), {
+        method: 'GET',
+    });
+    if (!response.ok) {
+        throw new Error('Ошибка запроса: ' + response.statusText);
+    }
+
+
+    const data = await response.json();
+    alert(data)
+    setUserCardGroup(data)
+setUserCardName(user.uName)
+    return JSON.stringify(data, null, 2);
+    } catch (error) {
+        console.error('Ошибка:', error);
+        alert('Ошибка: ' + error.message);
+    }
+}
+
+function setUserCardGroup(text) {
+  document.getElementById('userCardGroup').textContent = text;
+}
+
+function setUserCardName(text) {
+  document.getElementById('userCardName').textContent = text;
+}
